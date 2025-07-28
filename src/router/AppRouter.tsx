ed83b4d3d9d0afd4a335fd9ead866e0b7fc277e9
leehuanganh <lehoanganh230803@gmail.com>
2025-07-29 06:47:13 +0700

    add notification on header, manager dashboard

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -14,6 +14,7 @@ import ForgotPasswordPage from "../pages/ForgotPasswordPage";
 import ResetPasswordPage from "../pages/ResetPasswordPage";
 import ChatbotPage from "../pages/ChatbotPage";
 import UserProfilePage from "../pages/UserProfilePage";
+import ManagerDashboardPage from "../pages/ManagerDashboardPage";
 import StaffDashboardPage from "../pages/StaffDashboardPage";
 import StaffCoursesPage from "../pages/StaffCoursesPage";
 import StaffCourseDetailPage from "../pages/StaffCourseDetailPage";
@@ -134,6 +135,14 @@ const router = createBrowserRouter([
     element: (
       <ProtectedRoute requiredRoles={["STAFF", "ADMIN"]}>
         <StaffDashboardPage />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/manager/dashboard",
+    element: (
+      <ProtectedRoute requiredRoles={["MANAGER", "ADMIN"]}>
+        <ManagerDashboardPage />
       </ProtectedRoute>
     ),
   },
